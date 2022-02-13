b2ec6f714b41ff6c130e3f7b470ae4b3820d9bc6
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-13 02:55:11 +0100

    creating login page design

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -15,14 +15,158 @@ class _LoginPageState extends State<LoginPage> {
     double h = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            width: w,
-            height: h,
-            child: Lottie.asset('assets/LottieLogo1.json'),
-          )
-        ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                width: w,
+                height: h * 0.24,
+                child: Lottie.asset("assets/images/user.json"),
+              ),
+              SizedBox(
+                width: w,
+                height: h * 0.15,
+                child: Column(
+                  children: [
+                    Text(
+                      "Welcome back",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      child: Text(
+                        "Sign in with your account",
+                        style: TextStyle(
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black38),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        spreadRadius: 7,
+                        offset: Offset(1, 1),
+                        color: Colors.grey.withOpacity(0.2),
+                      )
+                    ]),
+                child: TextField(
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(color: Colors.white, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(color: Colors.white, width: 1.0),
+                      ),
+                      hintText: 'Enter Your Email ',
+                      prefixIcon: Icon(Icons.email_outlined)),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        spreadRadius: 6,
+                        offset: Offset(1, 1),
+                        color: Colors.grey.withOpacity(0.2),
+                      )
+                    ]),
+                child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        hintText: 'Enter Your password ',
+                        prefixIcon: Icon(Icons.password_outlined))),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: w * 0.5,
+                  ),
+                  Text(
+                    "Forgot your password ?",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black45),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.grey,
+                    elevation: 6,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30))),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    " Login ",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account create one ?",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black45),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Create one",
+                    style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
