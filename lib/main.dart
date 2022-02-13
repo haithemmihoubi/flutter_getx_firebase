73caf7dbcb431190a65738da03bc14178b7b5245
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-13 02:00:25 +0100

    adding user image json

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getx_firebase/views/homePage.dart';
+import 'package:flutter_getx_firebase/views/login_page.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -14,6 +14,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomePage());
+        home:LoginPage());
   }
 }
---
+++ lib/views/login_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            width: w,
+            height: h,
+            child: Lottie.asset('assets/LottieLogo1.json'),
+          )
+        ],
+      ),
+    );
+  }
+}
