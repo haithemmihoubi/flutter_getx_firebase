5d32a992a3c6a3632da6d366696048795fe32f3e
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-18 23:20:55 +0100

    creating splash screen

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class SplashSrcreen extends StatelessWidget {
+  const SplashSrcreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
