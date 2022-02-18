9277d083d132f9da5d81881b73674de1fba82a97
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-18 21:06:52 +0100

    initialising  firebase in the main.dart

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_getx_firebase/views/login_page.dart';
 import 'package:flutter_getx_firebase/views/welcomePage.dart';
 import 'package:get/get.dart';
@@ -7,7 +8,6 @@ class AuthController extends GetxController {
   final AuthController instance = Get.find();
 
   late Rx<User?> _user;
-
   FirebaseAuth auth = FirebaseAuth.instance;
 
   @override
@@ -15,7 +15,7 @@ class AuthController extends GetxController {
     super.onReady();
     _user = Rx<User?>(auth.currentUser);
     _user.bindStream(auth.userChanges());
-    ever(_user, _startScreen) ;
+    ever(_user, _startScreen);
   }
 
   _startScreen(User? user) {
@@ -27,15 +27,24 @@ class AuthController extends GetxController {
     }
   }
 
-
-  register(String email,password) {
-
-auth.createUserWithEmailAndPassword(email: email, password: password) ;
-
-
+  // Register method wit h email  and password using the firebase method
+  register(String email, password) {
+    try {
+      auth.createUserWithEmailAndPassword(email: email, password: password);
+    } catch (err) {
+      //catching errors and displaying it in  a snackbar
+      Get.snackbar("Aboout user", "user message",
+          backgroundColor: Colors.lightBlueAccent,
+          snackPosition: SnackPosition.BOTTOM,
+          titleText: Text(
+            "Account creation failed",
+            style: TextStyle(color: Colors.white),
+          ),
+          messageText: Text(
+            err.toString(),
+            style:
+                TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ));
+    }
   }
-
-
-
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_firebase/controllers/auth_controller.dart';
 import 'package:flutter_getx_firebase/views/login_page.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  //ensure that flutter is asynchronous and waits for the responses
+  WidgetsFlutterBinding.ensureInitialized();
+
+// firebase initialisation
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+
   runApp(MyApp());
 }
 
@@ -14,6 +22,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home:LoginPage());
+        home: LoginPage());
   }
 }
